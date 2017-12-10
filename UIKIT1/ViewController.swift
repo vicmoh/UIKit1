f7ae0415d068fd1df63bd327daa4c643311a0640
Vicky Mohammad <vimoh1213@gmail.com>
2017-12-09 21:11:27 -0500

    sdf

--- UIKIT1/ViewController.swift
+++ UIKIT1/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variable
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // Label
+        let label = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        //set the label text when the button is click
+        self.label = label
+        
+        // Button
+        let button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+        
+    }//end of view did load
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }//memory warning
 
+    func incrementCount() {
+        self.count = self.count + 1
+        self.label.text = "\(self.count)"
+    }//end increment function
 
-}
+}//end class
 
